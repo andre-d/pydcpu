bba428ca147db6486b4343cb7da94a24806e35df
Andre D <andre@andred.ca>
2012-03-29 19:17:29 -0400

    Moved loading and shutting down plugins into the plugin module

--- emu.py
+++ emu.py
@@ -14,39 +14,10 @@
 from dcpucore import DCPUCore
 import emuplugins
 
-def load_plugins(plugins, core):
-    if not plugins:
-        return plugins
-    print("Starting plugins")
-    loaded = []
-    for p in plugins:
-        loaded.append(p(core))
-    for l in loaded:
-        print("Starting plugin %s" % l.name)
-        l.start()
-    return loaded
-
-def shutdown_plugins(plugins):
-    if not plugins:
-        return
-    print("Shutting down plugins")
-    for p in plugins:
-        print("Shutting down %s" % p.name)
-        p.shutdown()
-    
-    shutdown = False
-    while not shutdown:
-        for p in plugins:
-            if p.is_alive():
-                print("Waiting for plugin %s to shutdown" % p.name)
-                continue;
-        shutdown = True
-
 def main():
     try:
-        plugins = emuplugins.plugins
         core = DCPUCore()
-        plugins = load_plugins(plugins, core)
+        plugins = emuplugins.load_plugins(core)
         running = True
 
         while True:
@@ -58,7 +29,7 @@ def main():
     except (KeyboardInterrupt, SystemExit):
         print("Shutting down")
     
-    shutdown_plugins(plugins)
+    emuplugins.shutdown_plugins(plugins)
     
     exit()
 
--- emuplugins/__init__.py
+++ emuplugins/__init__.py
@@ -1,4 +1,33 @@
-import config
+from emuplugins.emuplugin import EmuPlugin
+import emuconfig
 plugins = []
-for p in config.plugins:
+for p in emuconfig.plugins:
     plugins.append(__import__('emuplugins.%s'%p.lower(), globals(), locals(), [p], -1).__dict__[p])
+
+def load_plugins(*a, **kw):
+    if not plugins:
+        return plugins
+    print("Starting plugins")
+    loaded = []
+    for p in plugins:
+        loaded.append(p(*a, **kw))
+    for l in loaded:
+        print("Starting plugin %s" % l.name)
+        l.start()
+    return loaded
+
+def shutdown_plugins(plugins):
+    if not plugins:
+        return
+    print("Shutting down plugins")
+    for p in plugins:
+        print("Shutting down %s" % p.name)
+        p.shutdown()
+    
+    shutdown = False
+    while not shutdown:
+        for p in plugins:
+            if p.is_alive():
+                print("Waiting for plugin %s to shutdown" % p.name)
+                continue;
+        shutdown = True
