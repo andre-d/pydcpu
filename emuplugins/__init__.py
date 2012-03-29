f27dd4e13b18f1205848039cd6a8752b7b6d7636
Andre D <andre@andred.ca>
2012-03-29 19:10:44 -0400

    Dynamically loading plugins

--- emuplugins/__init__.py
+++ emuplugins/__init__.py
@@ -1,3 +1,4 @@
-from emuplugins.emuplugin import EmuPlugin
-from emuplugins.notchvid import NotchVid
-from config import plugins
+import config
+plugins = []
+for p in config.plugins:
+    plugins.append(__import__('emuplugins.%s'%p.lower(), globals(), locals(), [p], -1).__dict__[p])
